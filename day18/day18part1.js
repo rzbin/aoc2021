69bacee25fe61291254bc819445c8e8c1e5a60bc
rzbin <82371718+rzbin@users.noreply.github.com>
2021-12-18 11:01:27 +0100

    Day 18

---
+++ day18/day18part1.js
@@ -0,0 +1,89 @@
+const fs = require('fs')
+
+fs.readFile('input.txt', (err, buff) => {
+    if (err) {
+        console.error(err)
+        return
+    }
+    main(buff.toString())
+})
+
+function main(string) {
+    const numbers = string.split('\n').map(toFishNumber)
+
+    let num = numbers[0]
+    for (let i = 1; i < numbers.length; i++) {
+        console.log(' ', num.join(''))
+        console.log('+', numbers[i].join(''))
+        num = fishAdd(num, numbers[i])
+        console.log('=', num.join(''), '')
+        console.log()
+    }
+    console.log(getMagnitude(eval(num.join(''))))
+}
+function toFishNumber(string) {
+    const chars = string.split('')
+    const arr = []
+    for (let i = 0; i < chars.length; i++) {
+        if (chars[i] === '[' || chars[i] === ']' || chars[i] === ',') {
+            arr.push(chars[i])
+        } else {
+            let n = 0
+            while (Number(chars[i]) == chars[i]) {
+                n *= 10
+                n += Number(chars[i])
+                i++
+            }
+            i--
+            arr.push(n)
+        }
+    }
+    return arr
+}
+
+function fishAdd(a, b) {
+    return reduce(['[', ...a, ',', ...b, ']'])
+}
+
+function reduce(num) {
+    let depth = 0
+    let stop = false
+    for (let i = 0; i < num.length && !stop; i++) {
+        depth += num[i] === '[' ? 1 : 0
+        depth -= num[i] === ']' ? 1 : 0
+        if (depth > 4) {
+            stop = true
+            const [, l, , r] = num.splice(i, 5, 0)
+            for (let j = i - 1; j >= 0; j--) {
+                if (Number(num[j]) == num[j]) {
+                    num[j] += l
+                    break
+                }
+            }
+            for (let j = i + 1; j < num.length; j++) {
+                if (Number(num[j]) == num[j]) {
+                    num[j] += r
+                    break
+                }
+            }
+            break
+        }
+    }
+    for (let i = 0; i < num.length && !stop; i++) {
+        if (Number(num[i]) == num[i] && Number(num[i]) > 9) {
+            stop = true
+            const number = num[i] / 2
+            num.splice(i, 1, ...['[', Math.floor(number), ',', Math.ceil(number), ']'])
+            break
+        }
+    }
+
+    return stop ? reduce(num) : num
+}
+
+function getMagnitude(fish) {
+    if (Array.isArray(fish)) {
+        return 3 * getMagnitude(fish[0]) + 2 * getMagnitude(fish[1])
+    }
+    return fish
+}
